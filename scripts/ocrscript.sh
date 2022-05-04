d8c62eb1e26f7afea895ed85597f2a647a94cfb6
Matts Lindström <matts.lindstrom@abm.uu.se>
2022-05-04 10:41:43 +0200

    added scripts

---
+++ scripts/ocrscript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# copy this script, edit and run it in the directory where you keep image files that you want to ocr
+# requires tesseract-ocr and imagemagick (for image file conversions)
+
+# turns on verbose to get feedback:
+set -x
+IMG_FORMAT=jpg #use png, jpg, tiff -- or pdf but see below for that
+OCR_LANG=swe
+
+for i in *.$IMG_FORMAT
+do 
+	#*uncomment* these lines if you want to process single page pdfs:
+	#convert -density 300 -compress lzw $i temp.tif
+	#INPUT_FILE=temp.tif
+	# ... and *comment* this line (for pdf processing):
+	INPUT_FILE=$i
+	# Bash pattern ${VAR%pattern} – removes file extension from filename:
+	OUTPUT_BASENAME=${i%.*}
+	tesseract -l $OCR_LANG $INPUT_FILE $OUTPUT_BASENAME
+
+	# ...again, comment/uncomment depending on pdf or not
+	#rm temp.tif
+done
+set +x
