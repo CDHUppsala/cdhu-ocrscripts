dc8dec635522087b6dc15658b370488462d99442
Matts Lindström <matts.lindstrom@abm.uu.se>
2022-05-10 15:35:20 +0200

    added generate_swemper_yaml.sh, fixes

--- scripts/rename_all_pdfs_swemper.sh
+++ scripts/rename_all_pdfs_swemper.sh
@@ -1,26 +1,35 @@
-#!/bin/bash
+#!/bin/zsh
 # Author: ML/Authorfunction
 # Copy this script, edit and run it in the directory with pdfs
 # Requires: dflow2filename_swemper.py 
 
 # turn on verbose to get feedback:
 #set -x
-#IMG_FORMAT=jpg #use png, jpg, tiff -- or pdf but see below for that
-#OCR_LANG=swe
-CSV_SOURCE="../periodika_diabetes_dflowID.csv"
+CSV_SOURCE="./periodika_status_dflowID.csv"
+c=0
 
-for i in *.pdf
+for i in **/*.pdf
 do 
 	INPUT_FILE=$i
+	let "c++"
 	# Bash pattern ${VAR%pattern} – removes file extension from filename:
 	#OUTPUT_BASENAME=${i%.*}
-	#tesseract -l $OCR_LANG $INPUT_FILE $OUTPUT_BASENAME
 	NEW_FILENAME=$(./dflow2filename_swemper.py $CSV_SOURCE $INPUT_FILE)
-	#use echo for dry run...
-	#echo mv $INPUT_FILE $NEW_FILENAME
-	#use cp for security
-	cp $INPUT_FILE renamed/$NEW_FILENAME
 
+	if [[ "$1" == "-p" ]]
+	then
+		#Uncomment this to run hot:
+		: #nullcode so parsing continues if comment only (empty) block
+		#echo Uncomment hot code for production run!
+		#use cp for security
+		echo cp $INPUT_FILE renamed/$NEW_FILENAME
+	else
+		#echo commands for dry-run testing:
+		echo mv $INPUT_FILE $NEW_FILENAME	
+	fi
 
 done
+
+[ "$1" != "-p" ] && echo "Dry-run complete: Will rename/move $c files." || echo "Production run complete: Renamed/moved $c files."
+[ "$1" != "-p" ] && echo "Defaults to dry-run. Supply -p argument for production."
 set +x
