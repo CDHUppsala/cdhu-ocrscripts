51437b9e5ea4ccef753fca924727c64f43d2c1dd
Matts Lindström <matts.lindstrom@abm.uu.se>
2022-05-15 23:15:48 +0200

    ...

--- scripts/ocrscript_parallel.sh
+++ scripts/ocrscript_parallel.sh
@@ -13,22 +13,30 @@ CDHU="\
 "
 
 # turn on verbose to get feedback:
-#set -x
+set -x
 IMG_FORMAT=jpg
 OCR_LANG=swe
-DRY_ECHO=""
+DRY_ECHO=echo
 CAFFEINATE=""
-JOBS=6
+JOBS=6 #default to 6 threads/jobs, good for M1. Use option -j <jobs> to increase.
 
 echo $CDHU
 # Check if production run:
-[ "$1" != "-p" ] && DRY_ECHO=echo
+#[ "$1" != "-p" ] && DRY_ECHO=echo
 # Check if mac os; if true use caffeinate:
 [[ "$(uname)" == "Darwin" ]] && CAFFEINATE=caffeinate ; echo "On Mac OS, will run with caffeinate to avoid sleep"
 
+while getopts ":pj:" option; do
+   case $option in
+      p) # set production run by clearing DRY_ECHO
+         DRY_ECHO="";;
+	  j) JOBS=${OPTARG} #echo "j is ${OPTARG}";;
+   esac
+done
+
 find . -type f -name "*.jpg" \
 	| sed "s/\.jpg//" \
-	| $CAFFEINATE \time -h \
+	| $CAFFEINATE time \
 	parallel --bar -j $JOBS $DRY_ECHO tesseract -l $OCR_LANG {}.jpg {} #> /dev/null 2>&1
 
 [ "$1" != "-p" ] && echo "Defaults to dry-run. Supply -p argument for production."
