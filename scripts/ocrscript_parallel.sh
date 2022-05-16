21130d3af99491201724bffac8fcfc33f6ecb586
Matts Lindström <matts.lindstrom@abm.uu.se>
2022-05-16 02:42:18 +0200

    rewrote to fix progress bar and other things

--- scripts/ocrscript_parallel.sh
+++ scripts/ocrscript_parallel.sh
@@ -1,8 +1,7 @@
 #!/bin/zsh
 # Author: ML/Authorfunction
 # copy this script, edit and run it in the directory where you keep image files that you want to ocr
-# multicore version, requires gnu parallel
-# requires tesseract-ocr and imagemagick (for image file conversions)
+# multi/thread/core version of ocr script, requires gnu parallel
 CDHU="\
  ____ ____ ____ ____ ________ 
 ||C |||D |||H |||U |||       
@@ -16,29 +15,27 @@ CDHU="\
 #set -x
 IMG_FORMAT=jpg
 OCR_LANG=swe
-DRY_ECHO=echo
+DRY_ECHO="--dry-run"
 CAFFEINATE=""
-JOBS=6 #default to 6 threads/jobs, good for M1. Use option -j <jobs> to increase. JOBS=6 #default to 6 threads/jobs, good for M1. Use option -j <jobs> to increase.
+JOBS=6 #default to 6 threads/jobs, good for M1. Use option -j <jobs> to increase.
 export OMP_THREAD_LIMIT=1 #run single thread per job,  https://github.com/tesseract-ocr/tesseract/issues/3109
 
 echo $CDHU
-# Check if production run:
-#[ "$1" != "-p" ] && DRY_ECHO=echo
 # Check if mac os; if true use caffeinate:
 [[ "$(uname)" == "Darwin" ]] && CAFFEINATE=caffeinate ; echo "On Mac OS, will run with caffeinate to avoid sleep"
 
 while getopts ":pj:" option; do
 	case $option in
-		p) # set production run by clearing DRY_ECHO
+		p) # check/set production run by clearing DRY_ECHO
         	DRY_ECHO="";;
 		j) JOBS=${OPTARG} #echo "j is ${OPTARG}";;
 	esac
 done
 
-find . -type f -name "*.jpg" \
-	| sed "s/\.jpg//" \
-	| $CAFFEINATE time \
-	parallel --bar -j $JOBS $DRY_ECHO tesseract -l $OCR_LANG {}.jpg {} -c debug_file=/dev/null > /dev/null
+# SYNTAX GNU PARALLEL: parallel -j 8 convert {} -resize ... {} ::: *.png
+# after ::: is file list/regex. {.} means basefilename w/o extension
+$CAFFEINATE time parallel $DRY_ECHO --bar -j $JOBS '(tesseract -l '$OCR_LANG' "{}" "{.}" 2>/dev/null)' ::: *.$IMG_FORMAT
+# Hacked this example to suppress stderr and keep progress bar: '(ffmpegthumbnailer -i "{}" -o "/tmp/${DIR}/{}.jpg" -f 2>/dev/null)'  ::: *.mp4
 
 [ $DRY_ECHO ] && echo "Defaults to dry-run. Supply -p argument for production."
 set +x
